a3a051a71946559d35c47415626d842d71a71126
Garner <garnerj@stifel.com>
2018-01-02 11:33:04 -0600

    song edit css fixes

--- src/components/setlists/EditSong.js
+++ src/components/setlists/EditSong.js
@@ -1,43 +1,41 @@
 import React from 'react'
-import { observable } from 'mobx'
-import { observer, inject } from 'mobx-react'
+import {observable} from 'mobx'
+import {observer, inject} from 'mobx-react'
 import InputField from '../common/InputField'
 import TextField from '../common/TextField'
 
 @inject('store')
 @observer
 class EditSong extends React.Component {
-    constructor (props) {
+  constructor(props) {
     super(props)
     this.updateProperty = this.updateProperty.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  updateProperty (key, value) {
+  updateProperty(key, value) {
     this.props.item[key] = value
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const { item, store } = this.props
+    const {item, store} = this.props
     store.songs.update(item).then(() => {
       this.props.closeEdit();
     })
   }
 
   render() {
-    let{item} = this.props;
-    return (
-      <div className="list-entry">
+    let {item} = this.props;
+    return (<div className="song-detail-edit">
       <form onSubmit={this.handleSubmit}>
         <InputField name="title" value={item.title} onChange={this.updateProperty}/>
         <InputField name="artist" value={item.artist} onChange={this.updateProperty}/>
         <InputField name="key" value={item.key} onChange={this.updateProperty}/>
         <InputField name="videoURL" value={item.videoURL} onChange={this.updateProperty}/>
-        <input type="submit" value="save" />
+        <input type="submit" value="save"/>
       </form>
-      </div>
-    )
+    </div>)
   }
 }
 
